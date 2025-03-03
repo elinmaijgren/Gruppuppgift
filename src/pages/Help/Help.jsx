1fd4b5ba98602a1b61994953a59edf6a9736fb0b
David <david.olmos.verdugo@gmail.com>
2025-03-03 17:17:14 +0100

    Fixade footer, help.jsx samt home.css

--- src/pages/Help/Help.jsx
+++ src/pages/Help/Help.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import "./Help.css";
 
 const Help = () => {
-  return (
-    <div className="help-container">
-      <h1>Coming soon</h1>
-    </div>
-  );
+	return (
+		<div className="help-container">
+			<h1>Ska du ha hjälp nu också?</h1>
+			<p>
+				Om du behöver hjälp med något, kontakta oss på 08-123 456 78 eller
+				skicka ett mail till:{" "}
+			</p>
+			<a href="mailto:ViKommerInteSvara@mail.com">
+				ViKommerInteSvara@gmail.com
+			</a>
+		</div>
+	);
 };
 
 export default Help;
