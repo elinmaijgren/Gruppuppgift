7acb0705c6fb14f3185107b5dc2173924a8421d7
ahrirubin <ahri.rubin@gmail.com>
2025-03-03 16:13:59 +0100

    fixat nav?

--- src/pages/Product_info/Product_info.jsx
+++ src/pages/Product_info/Product_info.jsx
@@ -1,10 +1,12 @@
 import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 import api from "../../hooks/api";
 import "./Product_info.css";
 
 const Product_info = () => {
   const { id } = useParams();
-
+  const { addToCart } = useContext(CartContext);
   const { product, loading, error } = api(id);
 
   if (loading) return <p>Laddar produkt...</p>;
@@ -17,16 +19,8 @@ const Product_info = () => {
       <div className="product-info">
         <h1>{product.title}</h1>
         <p>{product.price} SEK</p>
-
         <p className="product-desc">{product.description}</p>
-
-        <button
-          onClick={() => {
-            console.log("Lägger till i kundvagn:", product);
-          }}
-        >
-          LÄGG TILL
-        </button>
+        <button onClick={() => addToCart(product)}>LÄGG TILL</button>
       </div>
     </div>
   );
